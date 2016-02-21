48ac449506672c941734d075b301f11cc096b4cd
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 09:48:09 +1300

    login steps done

--- db/week1/01_introduction/002_loginToDatabricks.scala
+++ db/week1/01_introduction/002_loginToDatabricks.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Sat, 20 Feb 2016 20:36:16 UTC
+// Databricks notebook source exported at Sun, 21 Feb 2016 20:48:01 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -21,11 +21,9 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC # Steps to get to databricks cloud:
+// MAGIC # Let us take the 7 Steps of the stairway to the databricks cloud
 // MAGIC 
-// MAGIC ### Step 1: go to [http://www.math.canterbury.ac.nz/databricks](http://www.math.canterbury.ac.nz/databricks)
-// MAGIC 
-// MAGIC ### Step 2: login using your email address and password given to you
+// MAGIC ### Step 1: go to [http://www.math.canterbury.ac.nz/databricks](http://www.math.canterbury.ac.nz/databricks) and login using your email address and temporary password given to you in person (now).
 
 // COMMAND ----------
 
@@ -38,6 +36,18 @@
 // MAGIC %md
 // MAGIC 
 // MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_02_sds_2016S1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### Step 2: Change your password immediately (now).
+// MAGIC 
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_pswdChange_sds_2016S1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_pswdChanged_sds_2016S1.png)
 
 // COMMAND ----------
 
@@ -57,6 +67,8 @@
 // MAGIC %md
 // MAGIC ### Step 5: name the cloned ``scalable-data-science`` folder as ``scalable-data-science`` 
 // MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_05_sds_2016S1.png)
+// MAGIC 
+// MAGIC **Note**: From week 2 onwards, you only need to clone the folder for that week (to preserve any changes you made to the notebooks from previous weeks).
 
 // COMMAND ----------
 
@@ -69,6 +81,22 @@
 // MAGIC %md
 // MAGIC 
 // MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_07_sds_2016S1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### Step 7: Attaching ``003_scalaCrashCourse`` notebook to the databricks cluster
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_08_sds_2016S1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_09_sds_2016S1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbLogin_10_sds_2016S1.png)
 
 // COMMAND ----------
 
