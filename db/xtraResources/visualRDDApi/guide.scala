b18cbdf3d1176a4ee5d0d82852dd371e3abb91da
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:14:11 +1300

    adding visualRDD guide

---
+++ db/xtraResources/visualRDDApi/guide.scala
@@ -0,0 +1,93 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:14:03 UTC
+// MAGIC %md
+// MAGIC # Here is a visual guide to the transformations and actions on RDDs
+// MAGIC 
+// MAGIC 
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-3.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-4.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-5.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-6.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-7.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-8.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-9.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-10.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-11.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Transformations
+// MAGIC 1. [map](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/transformations/map)
+// MAGIC * [filter](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/transformations/filter)
+// MAGIC * [flatMap](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/transformations/flatMap)
+// MAGIC * [groupBy](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/transformations/groupBy)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Continue from:
+// MAGIC 
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-46.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ***
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-85.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-86.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Actions
+// MAGIC 1. [getNumPartitions](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/actions/getNumPartitions)
+// MAGIC * [collect](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/actions/collect)
+// MAGIC * [reduce](/#workspace/scalable-data-science/xtraResources/visualRDDApi/recall/actions/reduce)
+
+// COMMAND ----------
+
