9b02141a6461876367008c682ab96b6f2f8a025d
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:20:09 +1300

    visual API groupBy

---
+++ db/xtraResources/visualRDDApi/recall/transformations/groupBy.scala
@@ -0,0 +1,48 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:20:01 UTC
+// MAGIC %md
+// MAGIC # groupBy
+// MAGIC 
+// MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-32.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-33.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-34.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-35.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-36.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-37.png)
+
+// COMMAND ----------
+
+val x = sc.parallelize(Array("John", "Fred", "Anna", "James"))
+val y = x.groupBy(w => w.charAt(0))
+
+// COMMAND ----------
+
+println(x.collect().mkString(", "))
+println(y.collect().mkString(", "))
+
+// COMMAND ----------
+
