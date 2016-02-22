1dd36a377f8d2cea7dea9eb88ea67cedae7bc70f
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:15:34 +1300

    visual RDDAPI collect

---
+++ db/xtraResources/visualRDDApi/recall/actions/collect.scala
@@ -0,0 +1,18 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:15:26 UTC
+// MAGIC %md
+// MAGIC # collect
+// MAGIC 
+// MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-89.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-90.png)
+
+// COMMAND ----------
+
