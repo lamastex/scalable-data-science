6f39d6137583a658b1e6702a7c98b4f6446a3dbd
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:17:51 +1300

    visual API reduce added

---
+++ db/xtraResources/visualRDDApi/recall/actions/reduce.scala
@@ -0,0 +1,38 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:17:43 UTC
+// MAGIC %md
+// MAGIC # reduce
+// MAGIC 
+// MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-91.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-92.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-93.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-94.png)
+
+// COMMAND ----------
+
+val x = sc.parallelize(Array(1,2,3,4))
+val y = x.reduce((a,b) => a+b)
+
+// COMMAND ----------
+
+println(x.collect.mkString(", "))
+println(y)
+
+// COMMAND ----------
+
