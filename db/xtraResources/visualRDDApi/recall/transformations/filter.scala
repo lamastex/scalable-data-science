c88c4f2c30c6d78dcced6c2cb698a29074a7ac66
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:18:38 +1300

    visual API filter added

---
+++ db/xtraResources/visualRDDApi/recall/transformations/filter.scala
@@ -0,0 +1,48 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:18:30 UTC
+// MAGIC %md
+// MAGIC # filter
+// MAGIC 
+// MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-19.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-20.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-21.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-22.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-23.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-24.png)
+
+// COMMAND ----------
+
+val x = sc.parallelize(Array(1,2,3))
+val y = x.filter(n => n%2 == 1)
+
+// COMMAND ----------
+
+println(x.collect().mkString(", "))
+println(y.collect().mkString(", "))
+
+// COMMAND ----------
+
