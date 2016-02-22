3b87953e1f6582a6d9f8a756276cf458fafebe4b
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-22 17:16:54 +1300

    visual API getNumPartitions added

---
+++ db/xtraResources/visualRDDApi/recall/actions/getNumPartitions.scala
@@ -0,0 +1,15 @@
+// Databricks notebook source exported at Mon, 22 Feb 2016 04:16:46 UTC
+// MAGIC %md
+// MAGIC # getNumPartitions
+// MAGIC 
+// MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-87.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-88.png)
