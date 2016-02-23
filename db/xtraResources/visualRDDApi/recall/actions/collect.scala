5a9e7f5ba1553e3b2506ed1f22f341fee553e936
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-23 20:46:22 +1300

    Update collect.scala

--- db/xtraResources/visualRDDApi/recall/actions/collect.scala
+++ db/xtraResources/visualRDDApi/recall/actions/collect.scala
@@ -1,6 +1,7 @@
-// Databricks notebook source exported at Mon, 22 Feb 2016 04:15:26 UTC
+// Databricks notebook source exported at Tue, 23 Feb 2016 07:46:12 UTC
 // MAGIC %md
 // MAGIC # collect
+// MAGIC Return all items in the RDD to the driver in a single list.
 // MAGIC 
 // MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
 
@@ -16,3 +17,14 @@
 
 // COMMAND ----------
 
+val x = sc.parallelize(Array(1,2,3), 2) // make a RDD with two partitions
+
+// COMMAND ----------
+
+// simply returns all elements in RDD x to the driver as an Array
+val y = x.collect()
+
+// COMMAND ----------
+
+//glom() flattens elements on the same partition
+val xOut = x.glom().collect() 
