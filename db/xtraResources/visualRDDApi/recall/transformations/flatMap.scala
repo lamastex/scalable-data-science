0482c1ca895ddf97cb6fafa626725511c05cda87
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-23 20:22:32 +1300

    Update flatMap.scala

--- db/xtraResources/visualRDDApi/recall/transformations/flatMap.scala
+++ db/xtraResources/visualRDDApi/recall/transformations/flatMap.scala
@@ -1,6 +1,7 @@
-// Databricks notebook source exported at Mon, 22 Feb 2016 04:19:17 UTC
+// Databricks notebook source exported at Tue, 23 Feb 2016 07:22:23 UTC
 // MAGIC %md
 // MAGIC # flatMap
+// MAGIC Return a new RDD by first applying a function to each element of this RDD and then flattening the results.
 // MAGIC 
 // MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
 
@@ -48,6 +49,3 @@ val y = x.flatMap(n => Array(n, n*100, 42))
 
 println(x.collect().mkString(", "))
 println(y.collect().mkString(", "))
-
-// COMMAND ----------
-
