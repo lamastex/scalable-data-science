8737a5c95d66b9587a19480f99540e34ba35e9b4
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-23 21:04:07 +1300

    Update getNumPartitions.scala

--- db/xtraResources/visualRDDApi/recall/actions/getNumPartitions.scala
+++ db/xtraResources/visualRDDApi/recall/actions/getNumPartitions.scala
@@ -1,6 +1,7 @@
-// Databricks notebook source exported at Mon, 22 Feb 2016 04:16:46 UTC
+// Databricks notebook source exported at Tue, 23 Feb 2016 08:03:57 UTC
 // MAGIC %md
 // MAGIC # getNumPartitions
+// MAGIC Return the number of partitions in RDD.
 // MAGIC 
 // MAGIC Let us look at the [legend and overview of the visual RDD Api](/#workspace/scalable-data-science/xtraResources/visualRDDApi/guide).
 
@@ -13,3 +14,16 @@
 
 // MAGIC %md
 // MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/visualapi/med/visualapi-88.png)
+
+// COMMAND ----------
+
+val x = sc.parallelize(Array(1,2,3), 2) // RDD with 2 partitions
+
+// COMMAND ----------
+
+val y = x.partitions.size
+
+// COMMAND ----------
+
+//glom() flattens elements on the same partition
+val xOut = x.glom().collect()
