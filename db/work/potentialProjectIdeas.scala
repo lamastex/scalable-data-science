c8538aa474aa2cde4d07f832f996418a1f481478
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-24 17:24:01 +1300

    list of potential projects for the course and a pointer in ACM communications, etc.

---
+++ db/work/potentialProjectIdeas.scala
@@ -0,0 +1,54 @@
+// Databricks notebook source exported at Wed, 24 Feb 2016 04:23:49 UTC
+// MAGIC %md
+// MAGIC # Here is a list of interesting papers from ACM that may help choose a project
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC ## Theoretical Projects in Scalable Data Science
+// MAGIC 1. Exact Matrix Completion via Convex Optimization
+// MAGIC  * **Abstract** 
+// MAGIC     - Suppose that one observes an incomplete subset of entries selected from a low-rank matrix. When is it possible to complete the matrix and recover the entries that have not been seen? We demonstrate that in very general settings, one can perfectly recover all of the missing entries from most sufficiently large subsets by solving a convex programming problem that finds the matrix with the minimum nuclear norm agreeing with the observed entries. The techniques used in this analysis draw upon parallels in the field of compressed sensing, demonstrating that objects other than signals and images can be perfectly reconstructed from very limited information.
+// MAGIC 
+// MAGIC  * As published in [DOI:10.1145/2184319.2184343](http://dl.acm.org/citation.cfm?id=2184319.2184343)
+// MAGIC  * Originally published in [FCM 9, 6 (2009)](http://link.springer.com/article/10.1007/s10208-009-9045-5#page-1)
+// MAGIC  
+// MAGIC * Data Science and Prediction
+// MAGIC  -  Big data promises automated actionable knowledge creation and predictive models for use by both humans and computers.
+// MAGIC  - [DOI:10.1145/2500499](http://dl.acm.org/citation.cfm?id=2500499)
+// MAGIC  
+// MAGIC 
+// MAGIC ## Applied Projects in Scalable Data Science
+// MAGIC  1. Content Recommendation on Web Portals
+// MAGIC   * How to offer recommendations to users when they have not specified what they want.
+// MAGIC   * [DOI:10.1145/2461256.2461277](http://dl.acm.org/citation.cfm?id=2461277)
+// MAGIC  2. Techniques and Applications for Sentiment Analysis
+// MAGIC   * The main applications and challenges of one of the hottest research areas in computer science.
+// MAGIC   * [DOI:10.1145/2436256.2436274](http://dl.acm.org/citation.cfm?id=2436274&picked=formats)
+// MAGIC  3. Looking back at big data (Digital humanities)
+// MAGIC   * As computational tools open up new ways of understanding history, historians and computer scientists are working together to explore the possibilities.
+// MAGIC   * [DOI:10.1145/2436256.2436263](http://dl.acm.org/citation.cfm?id=2436256.2436263)
+// MAGIC  4. Compitational Epidemiology
+// MAGIC   * The challenge of developing and using computer models to understand and control the diffusion of disease through populations.
+// MAGIC   * [DOI:10.1145/2483852.2483871](http://dl.acm.org/citation.cfm?id=2483871&picked=formats)
+// MAGIC  * Replicated Data Consistency Explained Through Baseball
+// MAGIC   * A broader class of consistency guarantees can, and perhaps should, be offered to clients that read shared data.
+// MAGIC   * [DOI:10.1145/2500500](http://dl.acm.org/citation.cfm?id=2500500)
+// MAGIC  * Big Data and its techincal challenges
+// MAGIC   * Exploring the inherent technical challenges in realizing the potential of Big Data.
+// MAGIC   * [DOI:10.1145/2611567](http://dl.acm.org/citation.cfm?id=2622628.2611567)
+// MAGIC   * (Extra reading)
+// MAGIC  * Community Sense and Response Systems: Your phone as a quake detector
+// MAGIC   * The Caltech CSN project collects sensor data from thousands of personal devices for real-time response to dangerous earthquakes.
+// MAGIC   * [DOI:10.1145/2622628.2622633](http://dl.acm.org/citation.cfm?id=2622633&dl=ACM&coll=DL&CFID=585754227&CFTOKEN=14268563)
+// MAGIC  * Big Data Meets Big Science
+// MAGIC   * Next-generation scientific instruments are forcing researchers to question the limits of massively parallel computing.
+// MAGIC   * [DOI:10.1145/2617660](http://dl.acm.org/citation.cfm?id=2617660)
+// MAGIC   * (Extra Reading)
+// MAGIC  * Reshaping Terrorist Networks
+// MAGIC   * To destabilize terrorist organizations, the <code>STONE</code> algorithms identify a set of operatives whose removal would maximally reduce lethality.
+// MAGIC   * [DOI:10.1145/2632661.2632664](http://dl.acm.org/citation.cfm?id=2632664)
+
+// COMMAND ----------
+
