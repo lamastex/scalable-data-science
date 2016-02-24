f62efb82504cbb6bc9ad12a0e6635da3e604341c
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-25 09:46:59 +1300

    aws Educate sharing

---
+++ db/xtraResources/awsEducate/sharing.scala
@@ -0,0 +1,136 @@
+// Databricks notebook source exported at Wed, 24 Feb 2016 20:46:47 UTC
+// MAGIC %md
+// MAGIC ## 21 Easy Steps  
+// MAGIC ### For sharing your quota of UC's AWS Educate credits towards the course's databricks cloud.
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 1
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000001.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 2
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000002.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 3
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000003.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 4
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000004.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 5
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000005.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 6
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000006.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 7
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000007.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 8
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000008.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 9
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000009.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 10
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000010.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 11
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000011.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 12
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000012.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 13
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000013.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 14
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000014.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 15
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000015.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 16
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000016.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 17
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000017.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 18
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000018.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 19
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000019.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 20
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000020.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Step 21
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000021.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## THANK YOU! 
+// MAGIC ### For sharing your quota of UC's AWS Educate credits towards the course's databricks cloud.
