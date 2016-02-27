2430628677ade20a0c2750bce68afa5d8f210bc2
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-28 12:25:38 +1300

    made 21 steps also local browsing-proof.

--- db/week1/01_introduction/002_loginToDatabricks.scala
+++ db/week1/01_introduction/002_loginToDatabricks.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Fri, 26 Feb 2016 07:15:47 UTC
+// Databricks notebook source exported at Sat, 27 Feb 2016 23:25:20 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -14,7 +14,11 @@
 
 // MAGIC %md
 // MAGIC # Outline
+// MAGIC 
 // MAGIC ### I. [21 Easy Steps for Sharing your AWS Educate Credits](/#workspace/scalable-data-science/xtraResources/awsEducate/sharing) (!! you HAVE to do this later !!)
+// MAGIC * if you are not in `*.cloud.databricks` and are browsing locally then go here for 
+// MAGIC     [here are the 21 easy steps for sharing your AWS Educate Credits](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/2016/S1/xtraResources/awsEducate/sharing.html)
+// MAGIC     
 // MAGIC ### II. 7 Steps to the Databricks Cloud
 // MAGIC ### III. Essentials of the Databricks Cloud
 
