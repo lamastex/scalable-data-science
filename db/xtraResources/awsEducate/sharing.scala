91f2b3851c14f7d06c0918f61efcb75365429048
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-02-28 20:36:23 +1300

    spam folder warnng added

--- db/xtraResources/awsEducate/sharing.scala
+++ db/xtraResources/awsEducate/sharing.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Sat, 27 Feb 2016 23:19:19 UTC
+// Databricks notebook source exported at Sun, 28 Feb 2016 07:36:05 UTC
 // MAGIC %md
 // MAGIC ## 21 Easy Steps  
 // MAGIC ### For sharing your quota of UC's AWS Educate credits towards the course's databricks cloud.
@@ -131,6 +131,34 @@
 // MAGIC ## Step 21
 // MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/awsEduShareIn21Steps_000021.png)
 // MAGIC 
+// MAGIC ## you will get a message from aws like so (CHECK your spam folder!)
+// MAGIC ```  
+// MAGIC   From: AWS Educate Support [support@awseducate.com]
+// MAGIC   Sent: Date
+// MAGIC   To: FirstName LastName
+// MAGIC   Subject: AWS Educate Application Approved
+// MAGIC 
+// MAGIC   Dear FirstName,
+// MAGIC 
+// MAGIC   Congratulations!
+// MAGIC 
+// MAGIC   Your AWS Educate application has been approved. As a member of the AWS Educate program, you will gain access to the benefits listed below:
+// MAGIC 
+// MAGIC   AWS Promotional Credit
+// MAGIC   It's our pleasure to issue you an Amazon Web Services (AWS) promotional credit code in the amount listed below.
+// MAGIC 
+// MAGIC   Credit Amount (US): $100.00
+// MAGIC   Credit Code: XXXXXXXXXXXXXX
+// MAGIC                
+// MAGIC   AWS Educate Student Collaboration Portal (SCP)
+// MAGIC   Click here for access to the Student Collaboration Portal (SCP), the hub for AWS Educate students around the world to learn more about the program, access 
+// MAGIC   self-paced labs and AWS certifications, and find AWS content to help with classwork.
+// MAGIC 
+// MAGIC   You can access a video walk-through of AWS Educate Students at: Click here
+// MAGIC 
+// MAGIC   Thank you again for participating in AWS Educate and we hope you enjoy the program!
+// MAGIC   The AWS Educate Team 
+// MAGIC   ```
 // MAGIC ## Now, please forward the Credit Code to your course coordinator by email 
 // MAGIC * with email's subject line "[aws credit code]"
 // MAGIC * and the email's body containing the Credit Code. In the above example the Credit Code is `CP3WEGXVI6X50C5`.
