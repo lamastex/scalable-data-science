4bead8cd877d4879f074c3daeb03b228a6e8027e
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-03-14 16:36:21 +1300

    adding Module 5 lectures of AJ's edX 2015 course from CE

---
+++ db/xtraResources/edXBigDataSeries2015/CS100-1x/Module 5: Lectures.py
@@ -0,0 +1,48 @@
+# Databricks notebook source exported at Mon, 14 Mar 2016 03:35:37 UTC
+# MAGIC %md
+# MAGIC **SOURCE:** This is from the Community Edition of databricks and has been added to this databricks shard at [/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x](/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x) as extra resources for the project-focussed course [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/) that is prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand), and *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+# MAGIC and 
+# MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome).
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Module Five Lecture
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Lecture 8: Exploratory Data Analysis and Machine Learning
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/37rz4y2X-O4')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/f0c1aPhcJ2s')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/pfGe4OnuiC8')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/h7-YY7Ijq48')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/l86sp4qwMLw')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/ru_J_794qac')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/0HGELVQSHb8')
