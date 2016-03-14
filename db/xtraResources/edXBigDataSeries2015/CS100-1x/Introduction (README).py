89309b230c67d13eb39f63b242d1f2d5bba3848f
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-03-14 16:14:44 +1300

    CC added from AJ's course.

--- db/xtraResources/edXBigDataSeries2015/CS100-1x/Introduction (README).py
+++ db/xtraResources/edXBigDataSeries2015/CS100-1x/Introduction (README).py
@@ -1,8 +1,13 @@
-# Databricks notebook source exported at Mon, 14 Mar 2016 02:32:05 UTC
+# Databricks notebook source exported at Mon, 14 Mar 2016 03:14:00 UTC
 # MAGIC %md
 # MAGIC **SOURCE:** This is from the Community Edition of databricks and has been added to this databricks shard at [/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x](/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x) as extra resources for the project-focussed course [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/) that is prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand), and *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 # MAGIC and 
 # MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome). 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
 
 # COMMAND ----------
 
