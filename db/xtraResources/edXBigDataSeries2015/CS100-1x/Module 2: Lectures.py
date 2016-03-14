09705740fdabf80d2e898cabedc754ce0c60a4b3
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-03-14 15:48:10 +1300

    adding AJ's with CC license... github asyncing 4th time...

--- db/xtraResources/edXBigDataSeries2015/CS100-1x/Module 2: Lectures.py
+++ db/xtraResources/edXBigDataSeries2015/CS100-1x/Module 2: Lectures.py
@@ -1,4 +1,4 @@
-# Databricks notebook source exported at Mon, 14 Mar 2016 02:45:49 UTC
+# Databricks notebook source exported at Mon, 14 Mar 2016 02:47:26 UTC
 # MAGIC %md
 # MAGIC **SOURCE:** This is from the Community Edition of databricks and has been added to this databricks shard at [/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x](/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x) as extra resources for the project-focussed course [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/) that is prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand), and *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 # MAGIC and 
@@ -6,7 +6,8 @@
 
 # COMMAND ----------
 
-
+# MAGIC %md
+# MAGIC <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
 
 # COMMAND ----------
 
