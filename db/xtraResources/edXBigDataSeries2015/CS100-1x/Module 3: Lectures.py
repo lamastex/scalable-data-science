3014fb3a3690ccc46aeb73bd8b1cd64f156ef90e
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-03-14 16:21:49 +1300

    adding AJ's edX 2015 course Module 3 lectures for structured data and SQL basic queries.. ideal complement for week3 of SDS

---
+++ db/xtraResources/edXBigDataSeries2015/CS100-1x/Module 3: Lectures.py
@@ -0,0 +1,105 @@
+# Databricks notebook source exported at Mon, 14 Mar 2016 03:21:05 UTC
+# MAGIC %md
+# MAGIC **SOURCE:** This is from the Community Edition of databricks and has been added to this databricks shard at [/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x](/#workspace/scalable-data-science/xtraResources/edXBigDataSeries2015/CS100-1x) as extra resources for the project-focussed course [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/) that is prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand), and *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+# MAGIC and 
+# MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome). 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Module Three Lectures
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Lecture 5: Semi-Structured Data
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/qzMs9Sq_DHw')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/pMSGGZVSwqo')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/NJyBQ-cQ3Ac')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/G_67yUxdDbU')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/Llof8ZgCHFE')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/KjzoBzCxHMs')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/25YMAapjJgw')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/otrnf8MQ8S8')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/8vpmMbmUAiA')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/Wc7zJG-N2B8')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/c2MFJI_NWVw')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Lecture 6: Structured Data
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/lODYQTgyqLk')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/BZuv__KF4qU')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/khFzRxjk2Tg')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/tAepBMlGvak')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/XAyWtVtBTlI')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/Zp0EF2Dghik')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/iAqgcaKERHM')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/kaX4I2jENJc')
+
+# COMMAND ----------
+
+displayHTML('https://youtube.com/embed/tBsNkJyFr2w')
