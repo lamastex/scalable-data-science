8d0d655b44f52a2e1d3db63a19b1cf881b00800f
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-03-14 13:43:35 +1300

    sql prog guide 000 scala-databricksified for SDS course.

---
+++ db/xtraResources/ProgGuides1_6/sqlProgrammingGuide/000_sqlProgGuide.scala
@@ -0,0 +1,70 @@
+// Databricks notebook source exported at Mon, 14 Mar 2016 00:42:51 UTC
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC 
+// MAGIC ### prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand)
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC This is an elaboration of the [Apache Spark 1.6 sql-progamming-guide](http://spark.apache.org/docs/latest/sql-programming-guide.html).
+// MAGIC 
+// MAGIC # [Spark Sql Programming Guide](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/000_sqlProgGuide)
+// MAGIC 
+// MAGIC -   [Overview](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/001_overview_sqlProgGuide)
+// MAGIC     -   SQL
+// MAGIC     -   DataFrames
+// MAGIC     -   Datasets
+// MAGIC -   [Getting Started](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/002_gettingStarted_sqlProgGuide)
+// MAGIC     -   Starting Point: SQLContext
+// MAGIC     -   Creating DataFrames
+// MAGIC     -   DataFrame Operations
+// MAGIC     -   Running SQL Queries Programmatically
+// MAGIC     -   Creating Datasets
+// MAGIC     -   Interoperating with RDDs
+// MAGIC         -   Inferring the Schema Using Reflection
+// MAGIC         -   Programmatically Specifying the Schema
+// MAGIC -   [Data Sources](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/003_dataSources_sqlProgGuide)
+// MAGIC     -   Generic Load/Save Functions
+// MAGIC         -   Manually Specifying Options
+// MAGIC         -   Run SQL on files directly
+// MAGIC         -   Save Modes
+// MAGIC         -   Saving to Persistent Tables
+// MAGIC     -   Parquet Files
+// MAGIC         -   Loading Data Programmatically
+// MAGIC         -   Partition Discovery
+// MAGIC         -   Schema Merging
+// MAGIC         -   Hive metastore Parquet table conversion
+// MAGIC             -   Hive/Parquet Schema Reconciliation
+// MAGIC             -   Metadata Refreshing
+// MAGIC         -   Configuration
+// MAGIC     -   JSON Datasets
+// MAGIC     -   Hive Tables
+// MAGIC         -   Interacting with Different Versions of Hive Metastore
+// MAGIC     -   JDBC To Other Databases
+// MAGIC     -   Troubleshooting
+// MAGIC -   [Performance Tuning](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/004_performanceTuning_sqlProgGuide)
+// MAGIC     -   Caching Data In Memory
+// MAGIC     -   Other Configuration Options
+// MAGIC -   [Distributed SQL Engine](/#workspace/scalable-data-science/xtraResources/ProgGuides1_6/sqlProgrammingGuide/005_distributedSqlEngine_sqlProgGuide)
+// MAGIC     -   Running the Thrift JDBC/ODBC server
+// MAGIC     -   Running the Spark SQL CLI
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC 
+// MAGIC ### prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand)
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
