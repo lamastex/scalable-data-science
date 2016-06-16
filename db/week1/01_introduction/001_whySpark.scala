76270899ba0a732acecb4014d2c0df489e4ab4bb
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-16 19:57:18 +1200

    html and uji added.

--- db/week1/01_introduction/001_whySpark.scala
+++ db/week1/01_introduction/001_whySpark.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 25 Feb 2016 02:58:16 UTC
+// Databricks notebook source exported at Thu, 16 Jun 2016 07:53:54 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -9,6 +9,13 @@
 // MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 // MAGIC and 
 // MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week1/01_introduction/000_scalableDataScience.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/week1/01_introduction/001_whySpark](http://img.youtube.com/vi/O8JbxgPpAU8/0.jpg)](https://www.youtube.com/v/O8JbxgPpAU8?rel=0&autoplay=1&modestbranding=1&start=2065&end=3330)
 
 // COMMAND ----------
 
@@ -101,8 +108,8 @@
 // MAGIC %md
 // MAGIC ***
 // MAGIC ***
-// MAGIC 20 minutes
-// MAGIC 40 out of 90+10 minutes.
+// MAGIC 55 minutes
+// MAGIC 55 out of 90+10 minutes.
 // MAGIC 
 // MAGIC We have come to the end of this section.
 // MAGIC 
