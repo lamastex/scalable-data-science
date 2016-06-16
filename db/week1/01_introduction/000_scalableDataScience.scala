b6783ccfb92450ae9519dc295be1c72b8cf26882
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-16 19:12:27 +1200

    html and uji added

--- db/week1/01_introduction/000_scalableDataScience.scala
+++ db/week1/01_introduction/000_scalableDataScience.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 16 Jun 2016 06:58:18 UTC
+// Databricks notebook source exported at Thu, 16 Jun 2016 07:09:03 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -13,7 +13,7 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC This notebook's recorded Uji(有時):
+// MAGIC The [html](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week1/01_introduction/000_scalableDataScience.html) of this databricks notebook and its recorded Uji(有時):
 // MAGIC 
 // MAGIC [![sds/uji/week1/01_introduction/000_scalableDataScience](http://img.youtube.com/vi/O8JbxgPpAU8/0.jpg)](https://www.youtube.com/v/O8JbxgPpAU8?rel=0&autoplay=1&modestbranding=1&start=0&end=2064)
 
