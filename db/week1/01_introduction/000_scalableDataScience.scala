c7f9e439ac0408e0f8f20b713d275a34a08295d9
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-16 19:13:05 +1200

    Update 000_scalableDataScience.scala

--- db/week1/01_introduction/000_scalableDataScience.scala
+++ db/week1/01_introduction/000_scalableDataScience.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 16 Jun 2016 07:09:03 UTC
+// Databricks notebook source exported at Thu, 16 Jun 2016 07:09:41 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
