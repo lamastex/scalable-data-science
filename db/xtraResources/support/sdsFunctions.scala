3b057c864e0c372db1996cffe04724778f326c02
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-17 15:56:36 +1200

    frameIt updated

--- db/xtraResources/support/sdsFunctions.scala
+++ db/xtraResources/support/sdsFunctions.scala
@@ -1,16 +1,18 @@
-// Databricks notebook source exported at Mon, 22 Feb 2016 04:12:15 UTC
+// Databricks notebook source exported at Fri, 17 Jun 2016 03:53:11 UTC
 // This is a sequence of functions that can be accessed by other notebooks using the following %run command:
 // %run "/scalable-data-science/xtraResources/support/sdsFunctions"
 
 // COMMAND ----------
 
+//OHOHNONOYAYASEEININGITBOOKNOW 
 //This allows easy embedding of publicly available information into any other notebook
+//when viewing in git-book just ignore this block - you may have to manually chase the URL in frameIt("URL").
 //Example usage:
 // displayHTML(frameIt("https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation#Topics_in_LDA",250))
 def frameIt( u:String, h:Int ) : String = {
       """<iframe 
  src=""""+ u+""""
- width="100%" height="""" + h + """"
+ width="95%" height="""" + h + """"
  sandbox>
   <p>
     <a href="http://spark.apache.org/docs/latest/ml-features.html">
