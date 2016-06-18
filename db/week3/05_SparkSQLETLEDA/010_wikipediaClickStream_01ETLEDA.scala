8152c9e8606c40b9ff47ecc2d514ef1cda5951b8
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-18 20:50:23 +1200

    ujified and frameIt fielded - '%md\n' added on Armbrust's strings

--- db/week3/05_SparkSQLETLEDA/010_wikipediaClickStream_01ETLEDA.scala
+++ db/week3/05_SparkSQLETLEDA/010_wikipediaClickStream_01ETLEDA.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Mon, 14 Mar 2016 04:49:20 UTC
+// Databricks notebook source exported at Sat, 18 Jun 2016 08:46:55 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -12,7 +12,15 @@
 
 // COMMAND ----------
 
-// MAGIC %md # <img width="300px" src="http://cdn.arstechnica.net/wp-content/uploads/2015/09/2000px-Wikipedia-logo-v2-en-640x735.jpg"/> Clickstream Analysis
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week3/05_SparkSQLETLEDA/010_wikipediaClickStream_01ETLEDA.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/week3/05_SparkSQLETLEDA/008_DiamondsPipeline](http://img.youtube.com/vi/6NoPvmTBVz0/0.jpg)](https://www.youtube.com/v/6NoPvmTBVz0?rel=0&autoplay=1&modestbranding=1&start=5402)
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC # <img width="300px" src="http://cdn.arstechnica.net/wp-content/uploads/2015/09/2000px-Wikipedia-logo-v2-en-640x735.jpg"/> Clickstream Analysis
 // MAGIC 
 // MAGIC ** Dataset: 3.2 billion requests collected during the month of February 2015 grouped by (src, dest) **
 // MAGIC 
@@ -58,10 +66,22 @@
 
 // COMMAND ----------
 
-// MAGIC %run "/scalable-data-science/xtraResources/support/sdsFunctions"
-
-// COMMAND ----------
-
+//This allows easy embedding of publicly available information into any other notebook
+//when viewing in git-book just ignore this block - you may have to manually chase the URL in frameIt("URL").
+//Example usage:
+// displayHTML(frameIt("https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation#Topics_in_LDA",250))
+def frameIt( u:String, h:Int ) : String = {
+      """<iframe 
+ src=""""+ u+""""
+ width="95%" height="""" + h + """"
+ sandbox>
+  <p>
+    <a href="http://spark.apache.org/docs/latest/index.html">
+      Fallback link for browsers that, unlikely, don't support frames
+    </a>
+  </p>
+</iframe>"""
+   }
 displayHTML(frameIt("https://datahub.io/dataset/wikipedia-clickstream",500))
 
 // COMMAND ----------
