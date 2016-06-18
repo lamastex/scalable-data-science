ee79a7b34a8ac82d43ece525a9981d405607d50b
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-18 17:13:09 +1200

    md double-checked

--- db/week2/03_WordCount/006_WordCount.scala
+++ db/week2/03_WordCount/006_WordCount.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 3 Mar 2016 22:25:04 UTC
+// Databricks notebook source exported at Sat, 18 Jun 2016 05:09:42 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -9,6 +9,13 @@
 // MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 // MAGIC and 
 // MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week2/03_WordCount/006_WordCount.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/week2/03_wordcount/006_wordcount](http://img.youtube.com/vi/zgkvusQdNLY/0.jpg)](https://www.youtube.com/v/zgkvusQdNLY?rel=0&autoplay=1&modestbranding=1&start=4613)
 
 // COMMAND ----------
 
