683da1f79e4a2ae385b2f2c3be9b0314cee81d52
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-18 22:26:39 +1200

    ujified

--- db/week4/06_MLIntro/011_IntroToML.scala
+++ db/week4/06_MLIntro/011_IntroToML.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 17 Mar 2016 03:10:33 UTC
+// Databricks notebook source exported at Sat, 18 Jun 2016 10:23:12 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -9,6 +9,13 @@
 // MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 // MAGIC and 
 // MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week4/06_MLIntro/011_IntroToML.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/week4/06_MLIntro/011_IntroToML](http://img.youtube.com/vi/_Lxtxmn0L-w/0.jpg)](https://www.youtube.com/v/_Lxtxmn0L-w?rel=0&autoplay=1&modestbranding=1&start=0&end=1494)
 
 // COMMAND ----------
 
@@ -120,7 +127,7 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC Take your own notes if you want ... (raaz may populate with characters later).
+// MAGIC Take your own notes if you want ....
 
 // COMMAND ----------
 
