eddfbbaf801167543e2664151d471b6c13f92030
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-19 14:58:29 +1200

    Update 000_dataTypesProgGuide.scala

--- db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/000_dataTypesProgGuide.scala
+++ db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/000_dataTypesProgGuide.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Tue, 22 Mar 2016 04:22:37 UTC
+// Databricks notebook source exported at Sun, 19 Jun 2016 02:55:00 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
