2512319bb57a10fbd809f0ff5cb8b22e49611e4b
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-19 15:03:41 +1200

    md rechecked

--- db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/001_LocalVector.scala
+++ db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/001_LocalVector.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Tue, 22 Mar 2016 04:17:45 UTC
+// Databricks notebook source exported at Sun, 19 Jun 2016 03:00:13 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
