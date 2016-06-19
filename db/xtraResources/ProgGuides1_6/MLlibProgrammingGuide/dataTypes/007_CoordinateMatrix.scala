7093e885d4a5f06d9eae441522507e989adc4412
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-19 15:09:18 +1200

    md checked

--- db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/007_CoordinateMatrix.scala
+++ db/xtraResources/ProgGuides1_6/MLlibProgrammingGuide/dataTypes/007_CoordinateMatrix.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Tue, 22 Mar 2016 04:21:02 UTC
+// Databricks notebook source exported at Sun, 19 Jun 2016 03:05:50 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
