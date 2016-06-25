60b6c67fd082585175d85e3389ea770e431d4562
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-25 15:14:34 +1200

    header/footer fixed

---
+++ db/studentProjects/02_AkinwandeAtanda/Tweet_Analytics/046_TA03_03_binary_classification_with_Loop.py
@@ -0,0 +1,95 @@
+# Databricks notebook source exported at Sat, 25 Jun 2016 03:10:56 UTC
+# MAGIC %md
+# MAGIC 
+# MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+# MAGIC 
+# MAGIC 
+# MAGIC ### Course Project by [Akinwande Atanda](https://nz.linkedin.com/in/akinwande-atanda)
+# MAGIC 
+# MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+# MAGIC and 
+# MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Creating Machine Learning Pipeline with Loop
+# MAGIC * The loop is created to test the sentitive of the designed algorithm to elasticNetParam
+# MAGIC * Read the Spark ML documentation for Logistic Regression
+# MAGIC * The dataset "pos_neg_category" can be split into two or three categories as done in previous note. In this note, the dataset is randomly split into training, validating and testing data
+# MAGIC * This notebook can be upload to create a job for scheduled training, validating and testing of the logistic classifier algorithm
+
+# COMMAND ----------
+
+from pyspark.ml import *
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import *
+from pyspark.ml.classification import *
+from pyspark.ml.tuning import *
+from pyspark.ml.evaluation import *
+from pyspark.ml.regression import *
+
+# COMMAND ----------
+
+df = table("pos_neg_category")
+
+# COMMAND ----------
+
+lrARValidate =[]
+lrARTest =[]
+param = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+for p in param:
+  bin = Binarizer(inputCol = "category", outputCol = "label", threshold = 0.5) # Positive reviews > 0.5 threshold
+  tok = Tokenizer(inputCol = "review", outputCol = "word") #Note: The column "words" in the original table can also contain sentences that will be tokenized
+  hashTF = HashingTF(inputCol = tok.getOutputCol(), numFeatures = 5000, outputCol = "features")
+  lr = LogisticRegression(maxIter = 10, regParam = 0.0001, elasticNetParam = p)
+  pipeline = Pipeline(stages = [bin, tok, hashTF, lr])
+  (trainingData, validateData, testData) = df.randomSplit([0.6, 0.3, 0.1])
+  model = pipeline.fit(trainingData)
+  validateModel=model.transform(validateData)
+  evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="precision")
+  accuracyValidateSet = evaluator.evaluate(validateModel)
+  testModel=model.transform(testData)
+  accuracyTestSet = evaluator.evaluate(testModel)
+#   print("Logistic Regression Classifier Accuracy Rate for Validation Dataset = %g " % (accuracyValidateSet))
+#   print("Logistic Regression Classifier Accuracy Rate for Test Dataset = %g " % (accuracyTestSet))
+#   print("Test Error = %g " % (1.0 - accuracy))
+  lrARValidate +=[(p,accuracyValidateSet)]
+  lrARTest +=[(p,accuracyTestSet)]
+
+# COMMAND ----------
+
+lrARValidate
+
+# COMMAND ----------
+
+lrARTest
+
+# COMMAND ----------
+
+print("Logistic Regression Classifier Accuracy Rate for Validation Dataset= ", lrARValidate)
+
+# COMMAND ----------
+
+print("Logistic Regression Classifier Accuracy Rate for Test Dataset= ", lrARTest)
+
+# COMMAND ----------
+
+#display(predictionModel.select("label","prediction", "words", "probability")) # Prob of being 0 (negative) against 1 (positive)
+
+# COMMAND ----------
+
+#predictionModel.select("label","prediction", "words", "probability").show(10) # Prob of being 0 (negative) against 1 (positive)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+# MAGIC 
+# MAGIC 
+# MAGIC ### Course Project by [Akinwande Atanda](https://nz.linkedin.com/in/akinwande-atanda)
+# MAGIC 
+# MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+# MAGIC and 
+# MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
