882c9299c7243d74fbda7448e0d7695eb7c0f4d1
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-25 17:58:39 +1200

    md'd

---
+++ db/studentProjects/00_studentPresentations.scala
@@ -0,0 +1,38 @@
+// Databricks notebook source exported at Sat, 25 Jun 2016 05:55:00 UTC
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC 
+// MAGIC ### prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) and [Sivanand Sivaram](https://www.linkedin.com/in/sivanand)
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Student Project Presentations
+
+// COMMAND ----------
+
+// MAGIC 
+// MAGIC %md
+// MAGIC 1. Dillon George 
+// MAGIC * [Akinwande Atanda](https://nz.linkedin.com/in/akinwande-atanda) (**todo: ** copy notebook from research shard to here)
+// MAGIC * Yinnon Dolev
+// MAGIC * Xin Zhao 
+// MAGIC * Shansha Zhou 
+// MAGIC * Shakira Suwan 
+// MAGIC * Matthew Hendlass 
+// MAGIC * Andrey Konstantinov: Keystroke Biometric
+// MAGIC * Dominic Lee: Random Matrices
+// MAGIC * Harry Wallace: Movie Recommender 
+// MAGIC * Ivan Sadikov: reading Netflow Files in Apache Spark
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC some nice news for next project...
+// MAGIC * https://databricks.com/blog/2016/05/24/genome-sequencing-in-a-nutshell.html
