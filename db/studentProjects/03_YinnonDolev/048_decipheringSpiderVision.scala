2c234c87b6db3509984014165a572da5627205ee
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-25 15:19:51 +1200

    Update 048_decipheringSpiderVision.scala

---
+++ db/studentProjects/03_YinnonDolev/048_decipheringSpiderVision.scala
@@ -0,0 +1,141 @@
+// Databricks notebook source exported at Sat, 25 Jun 2016 03:16:12 UTC
+// MAGIC %md
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC ## Deciphering Spider Vision 
+// MAGIC ### Student Project Presentation by Yinnon Dolev
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide1.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide2.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide3.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide4.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide5.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide6.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide7.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide8.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide8_1.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide9.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide10.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide11.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide12.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide13.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide14.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide15.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide15_1.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide16.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide17.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide18.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide19.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide20.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide21.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/03_YinnonDolev/images/Slide22.PNG)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC ## Student Project Presentation by Yinnon Dolev
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
