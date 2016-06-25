4ec5378e7cb6d780a13207281277999f23325bd0
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-25 16:40:43 +1200

    adding to git

---
+++ db/studentProjects/09_DominicLee/057_QuickReferences.scala
@@ -0,0 +1,59 @@
+// Databricks notebook source exported at Sat, 25 Jun 2016 04:37:04 UTC
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC 
+// MAGIC ### prepared by Dominic Lee
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ***
+
+// COMMAND ----------
+
+// MAGIC %md 
+// MAGIC #Quick References
+// MAGIC 
+// MAGIC [Databricks Guide](https://docs.cloud.databricks.com/docs/latest/databricks_guide/index.html#00%20Welcome%20to%20Databricks.html)
+// MAGIC 
+// MAGIC [LaTeX Guide](https://en.wikibooks.org/wiki/LaTeX)
+// MAGIC * [LaTeX Mathematics Guide](https://en.wikibooks.org/wiki/LaTeX/Mathematics)
+// MAGIC 
+// MAGIC [Linear Algebra Cheatsheet (Breeze/Matlab/Numpy/R)](https://github.com/scalanlp/breeze/wiki/Linear-Algebra-Cheat-Sheet)
+// MAGIC 
+// MAGIC [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+// MAGIC 
+// MAGIC Scala
+// MAGIC * [Scala Cheatsheet](http://docs.scala-lang.org/cheatsheets/)
+// MAGIC * [Scala Collections](http://docs.scala-lang.org/overviews/collections/introduction)
+// MAGIC 
+// MAGIC ScalaNLP Breeze
+// MAGIC * [ScalaNLP Breeze Wiki](https://github.com/scalanlp/breeze/wiki)
+// MAGIC * [ScalaNLP Breeze API](http://www.scalanlp.org/api/breeze/#package)
+// MAGIC 
+// MAGIC [Spark Programming Guide](https://spark.apache.org/docs/latest/programming-guide.html)  
+// MAGIC * [Spark MLLib Programming Guide](https://spark.apache.org/docs/latest/mllib-guide.html)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ***
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
+// MAGIC 
+// MAGIC 
+// MAGIC ### prepared by Dominic Lee
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+// MAGIC and 
+// MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
