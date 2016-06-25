9f433d60757e11789250feb437c0e86698777f5a
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-25 12:12:47 +1200

    ujified.

--- db/week6/12_SparkStreaming/023_TweetHashtagCount.scala
+++ db/week6/12_SparkStreaming/023_TweetHashtagCount.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Thu, 31 Mar 2016 03:17:52 UTC
+// Databricks notebook source exported at Sat, 25 Jun 2016 00:09:09 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -9,6 +9,13 @@
 // MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 // MAGIC and 
 // MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/week6/12_SparkStreaming/023_TweetHashtagCount.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/week6/12_SparkStreaming/023_TweetHashtagCount](http://img.youtube.com/vi/jqLcr2eS-Vs/0.jpg)](https://www.youtube.com/v/jqLcr2eS-Vs?rel=0&autoplay=1&modestbranding=1&start=3536&end=4519)
 
 // COMMAND ----------
 
