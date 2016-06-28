4af75e0ad85bed4f22da71cf3969d57b332f24f5
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-06-28 22:40:12 +1200

    ujified.

--- db/studentProjects/04_XinZhao/049_Introduction_HighOrderSpectralClustering.scala
+++ db/studentProjects/04_XinZhao/049_Introduction_HighOrderSpectralClustering.scala
@@ -1,4 +1,4 @@
-// Databricks notebook source exported at Sat, 25 Jun 2016 03:37:37 UTC
+// Databricks notebook source exported at Tue, 28 Jun 2016 10:36:27 UTC
 // MAGIC %md
 // MAGIC 
 // MAGIC # [Scalable Data Science](http://www.math.canterbury.ac.nz/~r.sainudiin/courses/ScalableDataScience/)
@@ -10,6 +10,13 @@
 // MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
 // MAGIC and 
 // MAGIC [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/AWS_logoTM_200px.png)](https://www.awseducate.com/microsite/CommunitiesEngageHome)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC The [html source url](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/db/studentProjects/04_XinZhao/049_Introduction_HighOrderSpectralClustering.html) of this databricks notebook and its recorded Uji ![Image of Uji, Dogen's Time-Being](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/UjiTimeBeingDogen.png "uji"):
+// MAGIC 
+// MAGIC [![sds/uji/studentProjects/04_XinZhao/049_Introduction_HighOrderSpectralClustering](http://img.youtube.com/vi/zJirlHAV6YU/0.jpg)](https://www.youtube.com/v/zJirlHAV6YU?rel=0&autoplay=1&modestbranding=1&start=3113&end=4018)
 
 // COMMAND ----------
 
