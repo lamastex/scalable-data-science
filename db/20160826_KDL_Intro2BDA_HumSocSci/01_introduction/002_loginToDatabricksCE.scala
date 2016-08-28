70b1691da9916991b6f5f0cdc025825430b4cd29
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-08-28 17:27:28 +0200

    saving KDL Workshop notebooks

---
+++ db/20160826_KDL_Intro2BDA_HumSocSci/01_introduction/002_loginToDatabricksCE.scala
@@ -0,0 +1,65 @@
+// Databricks notebook source exported at Sun, 28 Aug 2016 15:27:31 UTC
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Big Data Analysis for Humanities and Social Sciences](https://www.eventbrite.co.uk/e/big-data-analysis-for-the-humanities-and-social-sciences-tickets-26708754604)
+// MAGIC 
+// MAGIC ### August 26, 2016, King's Digital Lab, King's College London
+// MAGIC #### prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) 
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC # Working through the notebooks together!
+// MAGIC 
+// MAGIC * How many of you have databricks community edition accounts already?
+// MAGIC 
+// MAGIC * If you do then you can do the following:
+// MAGIC   * login
+// MAGIC   * start a cluster
+// MAGIC   * import this workshop .dbc file
+// MAGIC   * and try to follow along (for many of the notebooks) by doing as I do...
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # Essentials of Databricks Cloud (DBC)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: What is Databricks Cloud?
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_WorkspaceSparkPlatform700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: Shard, Cluster, Notebook and Dashboard
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_ShardClusterNotebookDashboard700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: Team, State, Collaboration, Elastic Resources
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_TeamStateCollaborationElasticResources700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Let us dive into Scala crash course in a notebook!
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC # [Big Data Analysis for Humanities and Social Sciences](https://www.eventbrite.co.uk/e/big-data-analysis-for-the-humanities-and-social-sciences-tickets-26708754604)
+// MAGIC 
+// MAGIC ### August 26, 2016, King's Digital Lab, King's College London
+// MAGIC #### prepared by [Raazesh Sainudiin](https://nz.linkedin.com/in/raazesh-sainudiin-45955845) 
+// MAGIC 
+// MAGIC *supported by* [![](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/databricks_logoTM_200px.png)](https://databricks.com/)
