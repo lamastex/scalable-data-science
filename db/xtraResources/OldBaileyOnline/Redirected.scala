6b895850c9286d1374545c031586a482328b8e23
Raazesh Sainudiin <raazesh-sainudiin@users.noreply.github.com>
2016-08-28 18:25:44 +0200

    adding KDL workshop notebook redirect

---
+++ db/xtraResources/OldBaileyOnline/Redirected.scala
@@ -0,0 +1,8 @@
+// Databricks notebook source exported at Sun, 28 Aug 2016 16:25:47 UTC
+// MAGIC %md
+// MAGIC 
+// MAGIC Old Bailey Online Analysis has been moved to after being updated at the King's Digital Lab Workshop on August 26 2016:
+// MAGIC * scalable-data-science/db/20160826_KDL_Intro2BDA_HumSocSci/06_OldBaileyOnline/OBO_LoadExtract.scala
+// MAGIC 
+// MAGIC It is available from this following github directory:
+// MAGIC * [https://github.com/raazesh-sainudiin/scalable-data-science/tree/master/db/20160826_KDL_Intro2BDA_HumSocSci/06_OldBaileyOnline](https://github.com/raazesh-sainudiin/scalable-data-science/tree/master/db/20160826_KDL_Intro2BDA_HumSocSci/06_OldBaileyOnline)
