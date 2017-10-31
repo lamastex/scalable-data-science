e124caea7c4a22577236811a4de6a1413fe4a7e3
raazesh-sainudiin <raaazesh.sainudiin@gmail.com>
2017-10-31 20:33:38 +0100

    adding dir to logging script

--- scripts/every2SecsRndWordsInFiles.sh
+++ scripts/every2SecsRndWordsInFiles.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-while true; do ruby -e 'a=STDIN.readlines;10.times do;b=[]; date = Time.now.to_s; b << date; b << Time.new.sec.to_s; 4.times do; b << a[rand(a.size)].chomp end; puts b.join(","); end;' < /usr/share/dict/words > $( date '+%S.log' ); sleep 2; done
+mkdir -p logsEvery2Secs &&
+while true; do ruby -e 'a=STDIN.readlines;10.times do;b=[]; date = Time.now.to_s; b << date; b << Time.new.sec.to_s; 4.times do; b << a[rand(a.size)].chomp end; puts b.join(","); end;' < /usr/share/dict/words > logsEvery2Secs/$( date '+%S.log' ); sleep 2; done
