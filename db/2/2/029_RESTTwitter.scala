a6961d51de2074b1e6282f4cd5fd1ebd66254aa8
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2017-11-14 11:35:27 +0100

    This is where twitter4j/scala REST API for Twitter will go

---
+++ db/2/2/029_RESTTwitter.scala
@@ -0,0 +1,13 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC 
+// MAGIC # Experimental Designs in Twitter
+// MAGIC 
+// MAGIC REST API and Streaming API - Is anyone interested in learning more?
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC - REST API
+// MAGIC - 
