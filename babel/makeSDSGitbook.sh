2e3f4d0ebc94506dca51e19e656b1c1bee2dd28c
raazesh-sainudiin <raaazesh.sainudiin@gmail.com>
2018-02-08 15:41:42 +0100

    adding shell script to generate the md for gitbook from dbc archive

---
+++ babel/makeSDSGitbook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#This will make a gitbook by generating markdown from the .dbc archive
+
+# cd to pinot directory
+PINOT_DIR=~/all/git/pinot
+sds_DIR=~/all/git/scalable-data-science
+
+cd $PINOT_DIR &&
+
+stack exec dbcflatten $sds_DIR/dbcArchives/2017/sds-2-2.dbc ~/all/git/scalable-data-science/gb/sds-2-2-flat.dbc &&
+
+stack exec -- pinot -f databricks -t markdown $sds_DIR/gb/sds-2-2-flat.dbc -o ~/all/git/scalable-data-science/gb/ &&
+
+rm $sds_DIR/gb/sds-2-2-flat.dbc &&
+
+rm -rf $sds_DIR/gb/db
+
+#mv ~/all/git/scalable-data-science/gb/sds-2-2/ ~/all/git/scalable-data-science/gb/db 
+
+
