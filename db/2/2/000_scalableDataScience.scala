71e27aa0ee4eed1337b813d030474ee3dbe50ff3
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-03-15 09:37:44 +0100

    vids linked

--- db/2/2/000_scalableDataScience.scala
+++ db/2/2/000_scalableDataScience.scala
@@ -6,9 +6,9 @@
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC Archived YouTube video of this live unedited lab-lecture:
+// MAGIC Archived YouTube videos of this live unedited lab-lecture:
 // MAGIC 
-// MAGIC [![Archived YouTube video of this live unedited lab-lecture](http://img.youtube.com/vi/zloLA6AyNqk/0.jpg)](https://www.youtube.com/embed/zloLA6AyNqk?start=0&end=3226&autoplay=1)
+// MAGIC [![Archived YouTube video of this live unedited lab-lecture](http://img.youtube.com/vi/zloLA6AyNqk/0.jpg)](https://www.youtube.com/embed/zloLA6AyNqk?start=0&end=3226&autoplay=1) [![Archived YouTube video of this live unedited lab-lecture](http://img.youtube.com/vi/36tQR9UPXP4/0.jpg)](https://www.youtube.com/embed/36tQR9UPXP4?start=0&end=2341&autoplay=1)
 
 // COMMAND ----------
 
