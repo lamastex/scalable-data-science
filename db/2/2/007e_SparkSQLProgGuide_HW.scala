39bdd2b98dc6d8ec40392b9b39bf353f33e01d67
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-03-15 10:50:39 +0100

    md

--- db/2/2/007e_SparkSQLProgGuide_HW.scala
+++ db/2/2/007e_SparkSQLProgGuide_HW.scala
@@ -34,10 +34,10 @@
 // MAGIC Configuration of in-memory caching can be done using the `setConf`
 // MAGIC method on `SparkSession` or by running `SET key=value` commands using SQL.
 // MAGIC 
-// MAGIC Property Name | Default | Meaning
-// MAGIC ---|---|---
-// MAGIC `spark.sql.inMemoryColumnarStorage.compressed` | true | When set to true Spark SQL will automatically select a compression codec for each column based on statistics of the data.
-// MAGIC `spark.sql.inMemoryColumnarStorage.batchSize` | 10000 | Controls the size of batches for columnar caching. Larger batch sizes can improve memory utilization and compression, but risk OOMs when caching data.
+// MAGIC | Property Name | Default | Meaning |
+// MAGIC | --- | --- | --- |
+// MAGIC | `spark.sql.inMemoryColumnarStorage.compressed` | true | When set to true Spark SQL will automatically select a compression codec for each column based on statistics of the data. |
+// MAGIC | `spark.sql.inMemoryColumnarStorage.batchSize` | 10000 | Controls the size of batches for columnar caching. Larger batch sizes can improve memory utilization and compression, but risk OOMs when caching data. |
 // MAGIC 
 // MAGIC Other Configuration Options
 // MAGIC ---------------------------
@@ -46,8 +46,8 @@
 // MAGIC execution. It is possible that these options will be deprecated in
 // MAGIC future release as more optimizations are performed automatically.
 // MAGIC 
-// MAGIC Property Name | Default | Meaning
-// MAGIC ---|---|---
-// MAGIC `spark.sql.autoBroadcastJoinThreshold` | 10485760 (10 MB) | Configures the maximum size in bytes for a table that will be broadcast to all worker nodes when performing a join. By setting this value to -1 broadcasting can be disabled. Note that currently statistics are only supported for Hive Metastore tables where the command `ANALYZE TABLE <tableName> COMPUTE STATISTICS noscan` has been run.
-// MAGIC `spark.sql.tungsten.enabled` | true | When true, use the optimized Tungsten physical execution backend which explicitly manages memory and dynamically generates bytecode for expression evaluation.
-// MAGIC `spark.sql.shuffle.partitions` | 200 | Configures the number of partitions to use when shuffling data for joins or aggregations.
+// MAGIC | Property Name | Default | Meaning |
+// MAGIC | ---|---|--- |
+// MAGIC |`spark.sql.autoBroadcastJoinThreshold` | 10485760 (10 MB) | Configures the maximum size in bytes for a table that will be broadcast to all worker nodes when performing a join. By setting this value to -1 broadcasting can be disabled. Note that currently statistics are only supported for Hive Metastore tables where the command `ANALYZE TABLE <tableName> COMPUTE STATISTICS noscan` has been run. |
+// MAGIC | `spark.sql.tungsten.enabled` | true | When true, use the optimized Tungsten physical execution backend which explicitly manages memory and dynamically generates bytecode for expression evaluation. |
+// MAGIC | `spark.sql.shuffle.partitions` | 200 | Configures the number of partitions to use when shuffling data for joins or aggregations. |
