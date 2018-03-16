b5635d4be74c2baf363ea1b81c7e56de9596cb5b
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-03-16 19:00:45 +0100

    md

--- db/2/2/999_04_StudentProject_PowerForecasting_1_LoadDataFromJSON.scala
+++ db/2/2/999_04_StudentProject_PowerForecasting_1_LoadDataFromJSON.scala
@@ -5,6 +5,13 @@
 
 // COMMAND ----------
 
+// MAGIC %md
+// MAGIC Archived YouTube video of this live unedited lab-lecture:
+// MAGIC 
+// MAGIC [![Archived YouTube video of this live unedited lab-lecture](http://img.youtube.com/vi/xqy5geCNKCg/0.jpg)](https://www.youtube.com/embed/xqy5geCNKCg?start=0&end=1456&autoplay=1)
+
+// COMMAND ----------
+
 // MAGIC %md 
 // MAGIC # Power Forecasting
 // MAGIC ## Student Project 
