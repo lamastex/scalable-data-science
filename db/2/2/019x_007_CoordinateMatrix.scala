67be87120bc127ce3a9779fde9571adc80ce29dc
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-03-16 10:45:57 +0100

    md

---
+++ db/2/2/019x_007_CoordinateMatrix.scala
@@ -0,0 +1,134 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC 
+// MAGIC # [SDS-2.2, Scalable Data Science](https://lamastex.github.io/scalable-data-science/sds/2/2/)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Archived YouTube video of this live unedited lab-lecture:
+// MAGIC 
+// MAGIC [![Archived YouTube video of this live unedited lab-lecture](http://img.youtube.com/vi/1NICbbECaC0/0.jpg)](https://www.youtube.com/embed/1NICbbECaC0?start=2285&end=2880&autoplay=1)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC This is an elaboration of the [Apache Spark mllib-progamming-guide on mllib-data-types](http://spark.apache.org/docs/latest/mllib-data-types.html).
+// MAGIC 
+// MAGIC # [Overview](/#workspace/scalable-data-science/xtraResources/ProgGuides2_2/MLlibProgrammingGuide/000_MLlibProgGuide)
+// MAGIC 
+// MAGIC ## [Data Types - MLlib Programming Guide](/#workspace/scalable-data-science/xtraResources/ProgGuides2_2/MLlibProgrammingGuide/dataTypes/000_dataTypesProgGuide)
+// MAGIC 
+// MAGIC -   [Local vector](http://spark.apache.org/docs/latest/mllib-data-types.html#local-vector)
+// MAGIC -   [Labeled point](http://spark.apache.org/docs/latest/mllib-data-types.html#labeled-point)
+// MAGIC -   [Local matrix](http://spark.apache.org/docs/latest/mllib-data-types.html#local-matrix)
+// MAGIC -   [Distributed matrix](http://spark.apache.org/docs/latest/mllib-data-types.html#distributed-matrix)
+// MAGIC     -   [RowMatrix](http://spark.apache.org/docs/latest/mllib-data-types.html#rowmatrix)
+// MAGIC     -   [IndexedRowMatrix](http://spark.apache.org/docs/latest/mllib-data-types.html#indexedrowmatrix)
+// MAGIC     -   [CoordinateMatrix](http://spark.apache.org/docs/latest/mllib-data-types.html#coordinatematrix)
+// MAGIC     -   [BlockMatrix](http://spark.apache.org/docs/latest/mllib-data-types.html#blockmatrix)
+// MAGIC 
+// MAGIC MLlib supports local vectors and matrices stored on a single machine, as
+// MAGIC well as distributed matrices backed by one or more RDDs. Local vectors
+// MAGIC and local matrices are simple data models that serve as public
+// MAGIC interfaces. The underlying linear algebra operations are provided by
+// MAGIC [Breeze](http://www.scalanlp.org/) and [jblas](http://jblas.org/). A
+// MAGIC training example used in supervised learning is called a “labeled point”
+// MAGIC in MLlib.
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### CoordinateMatrix in Scala
+// MAGIC 
+// MAGIC A `CoordinateMatrix` is a distributed matrix backed by an RDD of its
+// MAGIC entries. Each entry is a tuple of `(i: Long, j: Long, value: Double)`,
+// MAGIC where `i` is the row index, `j` is the column index, and `value` is the
+// MAGIC entry value. A `CoordinateMatrix` should be used only when both
+// MAGIC dimensions of the matrix are huge and the matrix is very sparse.
+// MAGIC 
+// MAGIC A [`CoordinateMatrix`](http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.mllib.linalg.distributed.CoordinateMatrix)
+// MAGIC can be created from an `RDD[MatrixEntry]` instance, where
+// MAGIC [`MatrixEntry`](http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.mllib.linalg.distributed.MatrixEntry)
+// MAGIC is a wrapper over `(Long, Long, Double)`. A `CoordinateMatrix` can be
+// MAGIC converted to an `IndexedRowMatrix` with sparse rows by calling
+// MAGIC `toIndexedRowMatrix`. Other computations for `CoordinateMatrix` are not
+// MAGIC currently supported.
+// MAGIC 
+// MAGIC Refer to the [`CoordinateMatrix` Scala docs](http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.mllib.linalg.distributed.CoordinateMatrix)
+// MAGIC for details on the API.
+
+// COMMAND ----------
+
+import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
+
+// COMMAND ----------
+
+val entries: RDD[MatrixEntry] = sc.parallelize(Array(MatrixEntry(0, 0, 1.2), MatrixEntry(1, 0, 2.1), MatrixEntry(6, 1, 3.7))) // an RDD of matrix entries
+
+// COMMAND ----------
+
+// Create a CoordinateMatrix from an RDD[MatrixEntry].
+val mat: CoordinateMatrix = new CoordinateMatrix(entries)
+
+// COMMAND ----------
+
+// Get its size.
+val m = mat.numRows()
+val n = mat.numCols()
+
+// COMMAND ----------
+
+// Convert it to an IndexRowMatrix whose rows are sparse vectors.
+val indexedRowMatrix = mat.toIndexedRowMatrix()
+
+// COMMAND ----------
+
+indexedRowMatrix.rows.collect()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### CoordinateMatrix in Scala
+// MAGIC 
+// MAGIC A [`CoordinateMatrix`](http://spark.apache.org/docs/latest/api/python/pyspark.mllib.html#pyspark.mllib.linalg.distributed.CoordinateMatrix)
+// MAGIC can be created from an `RDD` of `MatrixEntry` entries, where
+// MAGIC [`MatrixEntry`](http://spark.apache.org/docs/latest/api/python/pyspark.mllib.html#pyspark.mllib.linalg.distributed.MatrixEntry)
+// MAGIC is a wrapper over `(long, long, float)`. A `CoordinateMatrix` can be
+// MAGIC converted to a `RowMatrix` by calling `toRowMatrix`, or to an
+// MAGIC `IndexedRowMatrix` with sparse rows by calling `toIndexedRowMatrix`.
+// MAGIC 
+// MAGIC Refer to the [`CoordinateMatrix` Python docs](http://spark.apache.org/docs/latest/api/python/pyspark.mllib.html#pyspark.mllib.linalg.distributed.CoordinateMatrix)
+// MAGIC for more details on the API.
+
+// COMMAND ----------
+
+// MAGIC %py
+// MAGIC from pyspark.mllib.linalg.distributed import CoordinateMatrix, MatrixEntry
+// MAGIC 
+// MAGIC # Create an RDD of coordinate entries.
+// MAGIC #   - This can be done explicitly with the MatrixEntry class:
+// MAGIC entries = sc.parallelize([MatrixEntry(0, 0, 1.2), MatrixEntry(1, 0, 2.1), MatrixEntry(6, 1, 3.7)])
+// MAGIC 
+// MAGIC #   - or using (long, long, float) tuples:
+// MAGIC entries = sc.parallelize([(0, 0, 1.2), (1, 0, 2.1), (2, 1, 3.7)])
+// MAGIC 
+// MAGIC # Create an CoordinateMatrix from an RDD of MatrixEntries.
+// MAGIC mat = CoordinateMatrix(entries)
+// MAGIC 
+// MAGIC # Get its size.
+// MAGIC m = mat.numRows()  # 3
+// MAGIC n = mat.numCols()  # 2
+// MAGIC print (m,n)
+// MAGIC 
+// MAGIC # Get the entries as an RDD of MatrixEntries.
+// MAGIC entriesRDD = mat.entries
+// MAGIC 
+// MAGIC # Convert to a RowMatrix.
+// MAGIC rowMat = mat.toRowMatrix()
+// MAGIC 
+// MAGIC # Convert to an IndexedRowMatrix.
+// MAGIC indexedRowMat = mat.toIndexedRowMatrix()
+// MAGIC 
+// MAGIC # Convert to a BlockMatrix.
+// MAGIC blockMat = mat.toBlockMatrix()
