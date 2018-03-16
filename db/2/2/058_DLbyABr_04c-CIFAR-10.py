adaf1023702a8ad7904c92d3f475ab637e971494
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-03-16 18:15:53 +0100

    md

--- db/2/2/058_DLbyABr_04c-CIFAR-10.py
+++ db/2/2/058_DLbyABr_04c-CIFAR-10.py
@@ -54,6 +54,7 @@ except:
 # MAGIC Here is the code to unpickle the batches. 
 # MAGIC 
 # MAGIC Loaded in this way, each of the batch files contains a dictionary with the following elements:
+# MAGIC 
 # MAGIC * data - a 10000x3072 numpy array of uint8s. Each row of the array stores a 32x32 colour image. The first 1024 entries contain the red channel values, the next 1024 the green, and the final 1024 the blue. The image is stored in row-major order, so that the first 32 entries of the array are the red channel values of the first row of the image.
 # MAGIC * labels - a list of 10000 numbers in the range 0-9. The number at index i indicates the label of the ith image in the array data.
 
