28db5fde2a0c0b78de5f016115e3331212580a51
Raazesh Sainudiin <12589903+lamastex@users.noreply.github.com>
2018-04-21 19:56:41 +0200

    stable

---
+++ db/2/2/360-in-525-01/sds-2-2-360-in-525-01/002_loginToDatabricks.scala
@@ -0,0 +1,53 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # [SDS-2.2-360-in-525-01: Intro to Apache Spark for data Scientists](https://lamastex.github.io/scalable-data-science/360-in-525/2018/01/)
+// MAGIC ### [SDS-2.2, Scalable Data Science](https://lamastex.github.io/scalable-data-science/sds/2/2/)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## databricks community edition
+// MAGIC 
+// MAGIC 1. First obtain a free Obtain a databricks community edition account at:
+// MAGIC  * [https://community.cloud.databricks.com](https://community.cloud.databricks.com)
+// MAGIC 2. Let's get an overview of the databricks managed cloud for processing big data with Apache Spark
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Essentials of Databricks Cloud (DBC) in a Big Hurry
+// MAGIC 
+// MAGIC Please go here for a relaxed and detailed-enough tour (later):
+// MAGIC 
+// MAGIC * [https://docs.databricks.com/index.html](https://docs.databricks.com/index.html)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: What is Databricks Cloud?
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_WorkspaceSparkPlatform700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: Shard, Cluster, Notebook and Dashboard
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_ShardClusterNotebookDashboard700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## DBC Essentials: Team, State, Collaboration, Elastic Resources
+// MAGIC 
+// MAGIC ![DB workspace, spark, platform](https://raw.githubusercontent.com/raazesh-sainudiin/scalable-data-science/master/images/week1/dbTrImg_TeamStateCollaborationElasticResources700x.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC # You Should All Have databricks community edition account by now!
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Let us dive into Scala crash course in a notebook!
