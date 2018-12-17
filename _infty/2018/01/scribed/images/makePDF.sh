916078bdb7139453f60bb0684456e944859e7252
raazesh-sainudiin <raaazesh.sainudiin@gmail.com>
2018-12-17 10:26:33 +0100

    adding scribed notes

--- _infty/2018/01/scribed/images/makePDF.sh
+++ _infty/2018/01/scribed/images/makePDF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # from https://askubuntu.com/questions/493584/convert-images-to-pdf
 rm -r new.pdf
-ls *.jpeg | xargs -I% convert % -quality 60 %.pdf
-pdftk *.pdf cat output new.pdf && rm *.jpeg.pdf
+ls *.jpg | xargs -I% convert % -quality 70 %.pdf
+pdftk *.pdf cat output new.pdf && rm *.jpg.pdf
 #pdftk ../arch/soFar.pdf new.pdf cat output soFar.pdf
