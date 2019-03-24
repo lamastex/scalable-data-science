0ccfea1af634e0dd96efd001edbfeee6502b246e
Dan Lilja <dan@danlilja.se>
2019-03-24 15:52:00 +0100

    Added Zeppelin import script

---
+++ _sds/basics/infrastructure/onpremise/dockerCompose/zimport/zimport.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+
+import argparse
+import json
+import requests
+import time
+import os
+from os.path import isfile, join
+
+parser = argparse.ArgumentParser(description = "Import one or more Zeppelin \
+    notebooks into a running Zeppelin server. The imported files will be found \
+    in the folder set in ZEPPELIN_NOTEBOOK_DIR in the zeppelin-env.sh \
+    configuration file, defaulting to the notebook folder in the zeppelin \
+    root folder.")
+parser.add_argument('-a', '--host', default = 'localhost', dest = 'host',
+                    help = "Address of server running Zeppelin. Default: localhost")
+parser.add_argument('-p', '--port', default = '8080', dest = 'port',
+                    help = "Port used by Zeppelin. Default: 8080")
+parser.add_argument('notebook_dir', metavar = "Input directory",
+                    help = "Path to directory containing Zeppelin notebooks to be imported.")
+args = parser.parse_args()
+
+host = "http://{}:{}".format(args.host, args.port)
+
+def import_notebook(note):
+    requestURL = "{}/api/notebook/import".format(host)
+    r = requests.post(requestURL, data = note).json()
+    if r["status"] == "OK":
+        return r["body"]
+    else:
+        raise IOError(str(r))
+
+if __name__ == "__main__":
+    files = [join(args.notebook_dir, f) for f in os.listdir(args.notebook_dir)
+             if isfile(join(args.notebook_dir, f))]
+    for f in files:
+        json = open(f, "r").read()[1:]
+        import_notebook(json)
+
+
