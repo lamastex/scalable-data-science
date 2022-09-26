c9e97a4678cd9cb51840c53988f408abcd7744bb
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-26 17:50:45 +0200

    now MODULES env variable in env.list dictates the modules

--- actions/scripts/docker-run-pinot-mdbook.sh
+++ actions/scripts/docker-run-pinot-mdbook.sh
@@ -27,7 +27,8 @@ echo "done echoing env variables in use inside docker"
 #modules='000_0-sds-3-x-projects'
 modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 
-for module in $modules
+#from env list
+for module in $MODULES
 do
 
                                                                     #localdbcTEMPDIR = /home/runner/work/temp
