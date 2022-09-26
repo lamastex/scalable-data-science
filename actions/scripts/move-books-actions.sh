43767e078e7af718db90835e18c812205a345724
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 00:14:20 +0200

    adjusting

--- actions/scripts/move-books-actions.sh
+++ actions/scripts/move-books-actions.sh
@@ -7,6 +7,6 @@ for d in $modules
 do
     if [ $d != "src/" ] ; then
         mkdir -p $GITHUB_MDPUSHDIR/$d     #GITHUB_TEMPMDBOOKDIR = /home/runner/work/_temp/_github_home/temp/mdbooks
-        cp -r ${d}book/* $GITHUB_MDPUSHDIR/$d
+        cp -r ${d}/book/* $GITHUB_MDPUSHDIR/$d
     fi 
 done
