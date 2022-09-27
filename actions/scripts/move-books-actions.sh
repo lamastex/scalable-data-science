9621dca4e0d5a892cc01f8a777abe09cd2b25b9f
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 13:58:42 +0200

    adjusting

--- actions/scripts/move-books-actions.sh
+++ actions/scripts/move-books-actions.sh
@@ -4,7 +4,7 @@ cd $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science/mdbooks
 ls $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science/mdbooks
 modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 echo $MODULES
-for d in $MODULES
+for d in ${{ env.MODULES }}
 do
     if [ $d != "src/" ] ; then
         mkdir -p $GITHUB_MDPUSHDIR/$d     #GITHUB_TEMPMDBOOKDIR = /home/runner/work/_temp/_github_home/temp/mdbooks
