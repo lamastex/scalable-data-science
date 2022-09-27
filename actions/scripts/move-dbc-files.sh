be85de04d838078074f95d119ea6e74f1fb8710c
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 08:42:36 +0200

    adjusting

--- actions/scripts/move-dbc-files.sh
+++ actions/scripts/move-dbc-files.sh
@@ -19,7 +19,7 @@
 #modules='000_0-sds-3-x-projects'
 modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 #ls /home/runner/work/temp/
-cd /home/runner/work/temp/dbc
+cd /home/runner/work/temp/dbc/scalable-data-science
 ls
 echo $MODULES
 for module in $MODULES
