69df991cb7225a266e5c994e0aeec827a5593717
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 14:48:23 +0200

    adjusting

--- actions/scripts/docker-run-pinot-mdbook.sh
+++ actions/scripts/docker-run-pinot-mdbook.sh
@@ -32,12 +32,12 @@ modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 echo $MODULES
 for module in *
 do
-module=${module%_*}
+
 #ls -l $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science
                                                                     #localdbcTEMPDIR = /home/runner/work/temp
-docker run --rm  -i --name=haskell-pinot --env-file env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/haskell-pinot:latest /bin/bash $localscriptDIRPATH/pinotMdBook.sh $module
+docker run --rm  -i --name=haskell-pinot --env-file env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/haskell-pinot:latest /bin/bash $localscriptDIRPATH/pinotMdBook.sh basename $module
 
-docker run --rm  -i --name=rust-mdbook --env-file env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/rust-mdbook:latest /bin/bash $localscriptDIRPATH/rustMdBook.sh $module
+docker run --rm  -i --name=rust-mdbook --env-file env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/rust-mdbook:latest /bin/bash $localscriptDIRPATH/rustMdBook.sh basename $module
 done
 
 
