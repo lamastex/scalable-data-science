baca4b8ed6ae48ed4da998c4e8a16c1e82a2d9c2
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 15:26:27 +0200

    adjusting

--- actions/scripts/move-books-actions.sh
+++ actions/scripts/move-books-actions.sh
@@ -6,7 +6,7 @@ modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 echo $MODULES
 for d in *
 do
-d = basename $d .dbc
+#echo "${FILENAME%%.*}" >> noext
     if [ $d != "src/" ] ; then
         mkdir -p $GITHUB_MDPUSHDIR/$d     #GITHUB_TEMPMDBOOKDIR = /home/runner/work/_temp/_github_home/temp/mdbooks
         cp -r ${d}/book/* $GITHUB_MDPUSHDIR/$d
