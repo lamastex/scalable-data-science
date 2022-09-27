f616fd9fd7daca90db7165ccf0d9bfac13b99d55
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-09-27 15:33:02 +0200

    adjusting

--- actions/scripts/docker-run-pinot-mdbook.sh
+++ actions/scripts/docker-run-pinot-mdbook.sh
@@ -33,7 +33,7 @@ echo $MODULES
 for module in *
 do
 #module=basename $module .dbc
-echo "${FILENAME%%.*}" >> noext
+echo "${module%%.*}" >> noext
 #ls -l $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science
                                                                     #localdbcTEMPDIR = /home/runner/work/temp
 docker run --rm  -i --name=haskell-pinot --env-file env.list -v $GITHUB_TEMPMDBOOKDIR/dbc/scalable-data-science:/root/temp --mount type=bind,source=$GITHUB_DIR_TO_REPO,destination=/root/GIT lamastex/haskell-pinot:latest /bin/bash $localscriptDIRPATH/pinotMdBook.sh $noext
