3a4c007892c64cc8637483e09d46367878bcf7eb
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-04 20:44:12 +0100

    what the hell

--- actions/scripts/fetch_notebooks.sh
+++ actions/scripts/fetch_notebooks.sh
@@ -56,7 +56,6 @@ mkdir -p zipped
 
 for module in $modules
 do 
-cd $module
 rm -rf $module/*/
 # 000_1-sds-3-x-sql 000_2-sds-3-x-ml xtraResources 000_3-sds-3-x-st 000_4-sds-3-x-ss 000_5-sds-2-x-geo 000_6-sds-3-x-dl 000_7-sds-3-x-ddl 000_8-sds-3-x-pri 000_9-sds-3-x-trends do
 zip -r ${module}.dbc $module
