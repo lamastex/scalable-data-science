2d6f8f5f5b4de6cf00b995dec2437e289676260b
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-04 14:38:01 +0100

    first test

--- actions/scripts/fetch_notebooks.sh
+++ actions/scripts/fetch_notebooks.sh
@@ -48,12 +48,13 @@ find scalable-data-science/* -depth -name '*.py' -execdir rm {} \;
 pushd scalable-data-science
 #modules='000_5-sds-2-x-geo  000_4-sds-3-x-ss    000_3-sds-3-x-st'
 modules='000_1-sds-3-x-spark 000_1-sds-3-x-sql'
+#modules='000_1-sds-3-x-spark 000_1-sds-3-x-sql'
 
 #echo $MODULES
 echo $1
 mkdir -p zipped
 
-for module in $modules
+for module in *
 do 
 # 000_1-sds-3-x-sql 000_2-sds-3-x-ml xtraResources 000_3-sds-3-x-st 000_4-sds-3-x-ss 000_5-sds-2-x-geo 000_6-sds-3-x-dl 000_7-sds-3-x-ddl 000_8-sds-3-x-pri 000_9-sds-3-x-trends do
 zip -r ${module}.dbc $module
