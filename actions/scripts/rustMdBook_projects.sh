d65ff7b159591ad5d6d3ec850a3b07e4651ee9e3
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-05 10:44:57 +0100

    SUMMARY.md formatting

--- actions/scripts/rustMdBook_projects.sh
+++ actions/scripts/rustMdBook_projects.sh
@@ -14,10 +14,10 @@ pushd $MDBOOKdir/mdbooks/$1/src
 
 echo "we should find source and contents here"
 pwd
-echo "# Projects" > SUMMARY.md
+#echo "# Projects" > SUMMARY.md
 #echo "[Introduction](./README.md)" >> SUMMARY.md
 for d in contents/*; do
-#echo "# $(basename $d)">>SUMMARY.md
+echo "# $(basename $d)">>SUMMARY.md
 echo "">>SUMMARY.md
 find $d -iname '*.md' -type f | sort -h | while read f; do echo "   - ["$(basename $f .md)"](./$f)"; done > temp.md
 sed -i '1 s/^ *//' temp.md
