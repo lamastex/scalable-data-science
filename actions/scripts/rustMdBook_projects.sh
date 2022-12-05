574b0a9eef34f39bcc27918d1723a6543e5a8fcd
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-05 10:55:32 +0100

    SUMMARY.md formatting

--- actions/scripts/rustMdBook_projects.sh
+++ actions/scripts/rustMdBook_projects.sh
@@ -17,13 +17,16 @@ pwd
 #echo "# Projects" > SUMMARY.md
 #echo "[Introduction](./README.md)" >> SUMMARY.md
 for d in contents/*; do
-echo "# $(basename $d)">>SUMMARY.md
-echo "">>SUMMARY.md
+echo "# $(basename $d)">>temp.md
+echo "">>temp.md
 find $d -iname '*.md' -type f | sort -h | while read f; do echo "   - ["$(basename $f .md)"](./$f)"; done > temp.md
 sed -i '1 s/^ *//' temp.md
+echo "">>temp.md
+echo "---">>temp.md
+echo "">>temp.md
 cat temp.md >> SUMMARY.md
-echo "">>SUMMARY.md
-echo "---"
+
+#echo "---"
 
 done
 #find contents -iname '*.md' -type f | sort -h | while read f; do echo "- ["$(basename $f .md)"]($f)"; done > SUMMARY.md
@@ -34,7 +37,8 @@ done
 #cat bigSUMMARY.md | grep "${1}"  > SUMMARY.md
 
 #add editors
-echo "">>SUMMARY.md
+#echo "">>SUMMARY.md
+echo "---"
 echo "# Editors" > editors.md
 echo "Here is a list of the editors who have helped improve this book" >> editors.md
 echo "- [Raazesh Sainudiin](https://www.linkedin.com/in/raazesh-sainudiin-45955845/)" >> editors.md
