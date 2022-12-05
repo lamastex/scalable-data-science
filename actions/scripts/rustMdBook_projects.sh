c563ce17bd6d3eb4d0d39836ab337af44b15a9d0
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-05 09:46:19 +0100

    SUMMARY.md formatting

--- actions/scripts/rustMdBook_projects.sh
+++ actions/scripts/rustMdBook_projects.sh
@@ -20,8 +20,7 @@ for d in contents/*; do
 #echo "# $(basename $d)">>SUMMARY.md
 echo "">>SUMMARY.md
 find $d -iname '*.md' -type f | sort -h | while read f; do echo "   - ["$(basename $f .md)"](./$f)"; done >> SUMMARY.md
-echo "">>SUMMARY.md
-echo "---">>SUMMARY.md
+
 done
 #find contents -iname '*.md' -type f | sort -h | while read f; do echo "- ["$(basename $f .md)"]($f)"; done > SUMMARY.md
 cp SUMMARY.md bigSUMMARY.md
