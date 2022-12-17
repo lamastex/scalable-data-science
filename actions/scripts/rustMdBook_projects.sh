d3a0b1d17dcc0bea4533e482004b20bb34c756fc
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-12-17 15:11:25 +0100

    testing adding intro.readme

--- actions/scripts/rustMdBook_projects.sh
+++ actions/scripts/rustMdBook_projects.sh
@@ -14,7 +14,11 @@ pushd $MDBOOKdir/mdbooks/$1/src
 
 echo "we should find source and contents here"
 pwd
-echo "# Projects" > SUMMARY.md
+
+echo "# Summary" > SUMMARY.md
+echo "" >> SUMMARY.md
+echo "[Introduction](./introduction.md)" >> SUMMARY.md
+echo "# Projects" >> SUMMARY.md
 echo "" >> SUMMARY.md
 for d in contents/*; do
 #echo "# $(basename $d)">temp.md
